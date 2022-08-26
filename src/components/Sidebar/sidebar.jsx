c0de4ac4c826ee7469dc06dcdd7ca70d15126dae
Austin Perrine <akperrine@gmail.com>
2022-08-26 14:33:47 -0600

    search bar styling added

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -1,12 +1,40 @@
 import "./sidebar.css";
-import { Grid } from "@mui/material";
+import { Avatar, Grid, IconButton, Search } from "@mui/material";
+import { MdOutlineDonutLarge } from "react-icons/md";
+import { BsChatTextFill, BsThreeDotsVertical, BsSearch } from "react-icons/bs";
 
 const Sidebar = () => {
   return (
     <div className="sidebar-container">
       <Grid className="sidebar-header" container direction="row">
-        Hello
+        <Grid className="sidebar-avatar" item sm={2}>
+          <Avatar />
+        </Grid>
+        <Grid item sm={4} />
+        <Grid container item sm={6}>
+          <Grid item sm={3}>
+            <IconButton>
+              <MdOutlineDonutLarge className="sidebar-icon" />
+            </IconButton>
+          </Grid>
+          <Grid item sm={3}>
+            <IconButton>
+              <BsChatTextFill className="sidebar-icon" />
+            </IconButton>
+          </Grid>
+          <Grid item sm={3}>
+            <IconButton>
+              <BsThreeDotsVertical className="sidebar-icon" />
+            </IconButton>
+          </Grid>
+        </Grid>
       </Grid>
+      <div className="sidebar-search-container">
+        <div className="sidebar-search">
+          <BsSearch className="sidebar-search-icon" />
+          <input type="text" placeholder="search messages" />
+        </div>
+      </div>
     </div>
   );
 };
